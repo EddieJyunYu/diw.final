d74693b3b13e1733e29991b965652eb5bef8da20
EddieJyunYu <140496691+EddieJyunYu@users.noreply.github.com>
2023-08-11 00:00:51 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,52 @@
+$('#sp-men').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-women').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('#sp-kid').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// JavaScript Document
